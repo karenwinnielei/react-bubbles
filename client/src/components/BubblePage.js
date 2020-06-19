871b32db8748bd3e456680b3cb2480c65c6a4420
Karen Lei <karenwinnielei@gmail.com>
2020-06-19 08:43:32 -0700

    create private route, axiosWithAuth, login form, saveEdit, and deleteColor functions

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+    axiosWithAuth()
+      .get('colors')
+      .then(res => {
+        console.log(res)
+        setColorList(res.data)
+      })
+      .catch((err) =>
+        console.log(`Fetching colors error: ${err.response}`),
+      );
+  }, []);
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
